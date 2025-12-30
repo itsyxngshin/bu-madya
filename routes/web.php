42b0e4f04362d0cf1722dc9af86ac26be86a6e85
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-12-30 13:48:53 +0800

    chore: configure routes

--- routes/web.php
+++ routes/web.php
@@ -93,8 +93,6 @@ Route::middleware(['auth', 'role:administrator'])->prefix('admin')->name('admin.
 });
 
 Route::middleware(['auth', 'role:administrator', 'role:director'])->group(function () {
-    Route::get('admin/proposals/{proposal}', ProposalsShow::class)->name('admin.proposals.show');
-    Route::get('admin/proposals', ProposalsIndex::class)->name('admin.proposals.index');
     Route::get('/project/create', ProjectsCreate::class)->name('projects.create');
     Route::get('/projects/{project:slug}/edit', ProjectsEdit::class)->name('projects.edit');
     Route::get('/profile/edit', EditProfile::class)->name('profile.edit');
