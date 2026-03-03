a053ddf4e62c96a23d8181c37b2563f9eb1269ea
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-03-03 16:15:11 +0800

    fix: add features for the organization side

--- routes/web.php
+++ routes/web.php
@@ -164,8 +164,11 @@ Route::middleware(['auth', 'role:organization'])->prefix('partner')->name('partn
     Route::get('/dashboard', PartnerDashboard::class)->name('dashboard');
     Route::get('/submit-frame', SubmitFrame::class)->name('frames.submit');
     Route::get('/event/{event:slug}/registrants', EventRegistrants::class)->name('events.registrants');
+    Route::get('/events', AdminEventIndex::class)->name('events.index');
     Route::get('/event/create', CreateEvent::class)->name('events.create');
+    Route::get('/events/{id}/edit', EditEvent::class)->name('events.edit');
     Route::get('/event/{event}/edit', EditEvent::class)->name('events.edit');
+    Route::get('/events/{event:slug}/raffle', EventRaffle::class)->name('events.raffle');
     
 });
 
