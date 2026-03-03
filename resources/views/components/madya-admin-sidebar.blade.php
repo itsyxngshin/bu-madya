62d7cf2ff01e69af0200fda32709a2a7ac21ea81
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-03-03 16:13:26 +0800

    fix: reroute link in sidebar on organization's end

--- resources/views/components/madya-admin-sidebar.blade.php
+++ resources/views/components/madya-admin-sidebar.blade.php
@@ -55,9 +55,9 @@
         </a>
 
         {{-- 1. EVENTS --}}
-        <a href="{{ route('admin.events.index') }}"
-           class="{{ $linkClass }} {{ request()->routeIs('admin.events.*') ? $activeClass : $inactiveClass }}">
-            <svg class="w-5 h-5 {{ request()->routeIs('admin.events.*') ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"> </path></svg>
+        <a href="{{ route('open.events.index') }}"
+           class="{{ $linkClass }} {{ request()->routeIs('open.events.*') ? $activeClass : $inactiveClass }}">
+            <svg class="w-5 h-5 {{ request()->routeIs('open.events.*') ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"> </path></svg>
             Manage Events
         </a>
 
