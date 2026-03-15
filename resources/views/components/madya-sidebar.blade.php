85dd23e28589f8abb9578e21bfde5d1279025bb9
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-03-15 19:21:35 +0800

    fix: add evaluations to the directors sidebar

--- resources/views/components/madya-sidebar.blade.php
+++ resources/views/components/madya-sidebar.blade.php
@@ -67,7 +67,7 @@
             Projects
         </a>
 
-        <a href="{{ route('director.evaluations.index') }}" class="{{ $linkClass }} {{ request()->routeIs('admin.evaluations.*') ? $activeClass : $inactiveClass }}">
+        <a href="{{ route('director.evaluations.index') }}" class="{{ $linkClass }} {{ request()->routeIs('director.evaluations.*') ? $activeClass : $inactiveClass }}">
                 <svg class="w-5 h-5 {{ request()->routeIs('director.evaluations.*') ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                 Evaluation Manager
             </a>
