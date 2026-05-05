089cc4db380bed0c511fcc30d41f0bbfeb28307b
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-05-05 11:22:48 +0800

    fix: add dashboard for the webmaster

---
+++ app/Livewire/Webmaster/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Webmaster;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.webmaster.dashboard');
+    }
+}
