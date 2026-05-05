978233a3522922e795b4ad8c8241a0bf760c75fc
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-05-05 11:09:57 +0800

    fix: modify time log models

--- app/Models/OjtBlog.php
+++ app/Models/OjtBlog.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OjtBlog extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'report_date',
+        'title',
+        'content',
+        'attachment_path', // Don't forget this for the photo uploads!
+    ];
+
+    protected $casts = [
+        'report_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/TimeLog.php
+++ app/Models/TimeLog.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TimeLog extends Model
 {
-    //
+    use HasFactory;
+
+    // 1. Allow Mass Assignment
+    protected $fillable = [
+        'user_id',
+        'log_date',
+        'morning_in',
+        'morning_out',
+        'afternoon_in',
+        'afternoon_out',
+        'total_minutes_rendered',
+        'status',
+    ];
+
+    // 2. Cast to Carbon Datatypes
+    protected $casts = [
+        'log_date' => 'date',
+        'morning_in' => 'datetime',
+        'morning_out' => 'datetime',
+        'afternoon_in' => 'datetime',
+        'afternoon_out' => 'datetime',
+        'total_minutes_rendered' => 'integer',
+    ];
+
+    // 3. Setup the Inverse Relationship
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
