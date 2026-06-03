b99de3921b03561665caab7022a3c44f6f570dc8
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-06-03 19:08:17 +0800

    fix: add admin access to meeting minutes

--- resources/views/components/madya-admin-sidebar.blade.php
+++ resources/views/components/madya-admin-sidebar.blade.php
@@ -139,6 +139,13 @@
                 <svg class="{{ $isEvaluationsActive ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                 Evaluation Manager
             </a>
+
+            {{-- NEW: Meeting Proceedings --}}
+            @php $isMeetingsActive = request()->routeIs('partner.meetings'); @endphp
+            <a href="{{ route('partner.meetings') }}" class="{{ $linkClass }} {{ $isMeetingsActive ? $activeClass : $inactiveClass }}">
+                <svg class="{{ $isMeetingsActive ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                Meeting Proceedings
+            </a>
         @endif
 
         @if($isAdmin)
@@ -160,13 +167,6 @@
             <a href="{{ route('partner.frames.submit') }}" class="{{ $linkClass }} {{ $isFramesActive ? $activeClass : $inactiveClass }}">
                 <svg class="{{ $isFramesActive ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                 My Campaign Frames
-            </a>
-
-            {{-- NEW: Meeting Proceedings --}}
-            @php $isMeetingsActive = request()->routeIs('partner.meetings'); @endphp
-            <a href="{{ route('partner.meetings') }}" class="{{ $linkClass }} {{ $isMeetingsActive ? $activeClass : $inactiveClass }}">
-                <svg class="{{ $isMeetingsActive ? $iconActive : $iconInactive }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                Meeting Proceedings
             </a>
         @endif
 
