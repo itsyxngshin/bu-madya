a4e4158e67ad78251b2df1f0d4772f7ce694966b
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-07-06 15:19:25 +0800

    fix: resolve conflict with web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\EnsureAccountIsActive::class, // <--- Add this line
         ]);
+        // Intercept unauthenticated users and route them correctly
+        $middleware->redirectGuestsTo(fn (Request $request) =>
+            $request->is('launchpad*') ? route('ibalong.login') : route('login')
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
