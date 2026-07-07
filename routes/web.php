3c51ac3963b6db279f4925b2f0f10727c245f7aa
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-07-07 22:10:29 +0800

    fix: add about section

--- routes/web.php
+++ routes/web.php
@@ -153,6 +153,7 @@ Route::domain('ibalong.' . env('APP_DOMAIN'))->name('ibalong.')->group(function 
     Route::get('/launchpad/login', \App\Livewire\Ibalong\Auth\Login::class)->name('login');
     // Place this outside of your auth middleware!
     Route::get('/roster', \App\Livewire\Ibalong\MeetTheTeam::class)->name('roster');
+    Route::get('/about', \App\Livewire\Ibalong\AboutHackathon::class)->name('about');
     Route::middleware(['ibalong.auth'])->group(function () {
 
         // Default Dashboard Overview
